c0ea37d0e98adcbbbdbd321344b7561d29c7d621
AliSinaSaemi <a.s.a.j.2016@gmail.com>
2017-10-26 23:55:58 +0800

    First commit.

---
+++ plugins/main/js/main.js
@@ -0,0 +1,61 @@
+/*=======================
+
+        Global
+ 
+=======================*/
+$(window).click(function (e) {
+  e.preventDefault();
+});
+
+/*=======================
+
+        sticky nav
+ 
+=======================*/
+$(document).ready(function () {
+  $(".navbar").sticky({
+    topSpacing: 0
+  });
+});
+/*=======================
+
+        sidenav.js
+ 
+=======================*/
+$('.sa-header-button').click(function (e) {
+  e.preventDefault();
+  $('.cssmenu').addClass('cssmenuShow');
+});
+
+$('.sa-close-btn').click(function () {
+  $('.cssmenu').removeClass('cssmenuShow');
+});
+
+/*=======================
+
+        hide/ show
+        nav
+ 
+=======================*/
+(function ($) {
+  $(document).ready(function () {
+    $('.cssmenu > ul > li > a').click(function () {
+      $('.cssmenu li').removeClass('active');
+      $(this).closest('li').addClass('active');
+      var checkElement = $(this).next();
+      if ((checkElement.is('ul')) && (checkElement.is(':visible'))) {
+        $(this).closest('li').removeClass('active');
+        checkElement.slideUp('normal');
+      }
+      if ((checkElement.is('ul')) && (!checkElement.is(':visible'))) {
+        $('.cssmenu ul ul:visible').slideUp('normal');
+        checkElement.slideDown('normal');
+      }
+      if ($(this).closest('li').find('ul').children().length == 0) {
+        return true;
+      } else {
+        return false;
+      }
+    });
+  });
+})(jQuery);
