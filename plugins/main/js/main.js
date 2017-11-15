360b9a4d989ac46e128225bdda879bcb3a0ed66c
AliSinaSaemi <a.s.a.j.2016@gmail.com>
2017-11-15 23:20:43 +0330

    Done is done !

--- plugins/main/js/main.js
+++ plugins/main/js/main.js
@@ -17,22 +17,47 @@ $(document).ready(function () {
     topSpacing: 0
   });
 });
+/*=======================
+
+        back to top
+ 
+=======================*/
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 50) {
+      $('#back-to-top').fadeIn();
+    } else {
+      $('#back-to-top').fadeOut();
+    }
+  });
+  // scroll body to 0px on click
+  $('#back-to-top').click(function () {
+    $('#back-to-top').tooltip('hide');
+    $('body,html').animate({
+      scrollTop: 0
+    }, 800);
+    return false;
+  });
+
+});
 
 /*=======================
 
         accordion.js
  
 =======================*/
-$(document).ready(function() {
+$(document).ready(function () {
   $('.collapse.in').prev('.panel-heading').addClass('active');
   $('#accordion, #bs-collapse')
-    .on('show.bs.collapse', function(a) {
+    .on('show.bs.collapse', function (a) {
       $(a.target).prev('.panel-heading').addClass('active');
     })
-    .on('hide.bs.collapse', function(a) {
+    .on('hide.bs.collapse', function (a) {
       $(a.target).prev('.panel-heading').removeClass('active');
     });
 });
+
+
 
 /*=======================
 
